225c1b515ac33fc9cf873b1d20df935eb445a1a0
Ayo <ayo@ayco.io>
2026-04-02 17:50:59 +0200

    chore: update release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,13 +1,17 @@
-// from https://github.com/elk-zone/elk/blob/main/scripts/release.ts
+// @ts-check
+// forked from https://github.com/elk-zone/elk/blob/main/scripts/release.ts
 
 import Git from 'simple-git'
 
+
 const git = Git()
+const upstream = 'gh'
 
 const hash = await git.revparse(['main'])
 
 console.log('Checkout release branch')
-await git.checkout('release')
+await git.fetch('gh')
+await git.checkout(`${upstream}/release`, {l})
 
 console.log(`Reset to main branch (${hash})`)
 await git.reset(['--hard', hash])
