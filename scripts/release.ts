a41604e3b2aa198092720bab85cc02638733e00f
Ayo <ayo@ayco.io>
2026-04-02 17:32:16 +0200

    chore: new release script inspired by how we do it on Elk.zone

---
+++ scripts/release.ts
@@ -0,0 +1,19 @@
+// from https://github.com/elk-zone/elk/blob/main/scripts/release.ts
+
+import Git from 'simple-git'
+
+const git = Git()
+
+const hash = await git.revparse(['main'])
+
+console.log('Checkout release branch')
+await git.checkout('release')
+
+console.log(`Reset to main branch (${hash})`)
+await git.reset(['--hard', hash])
+
+console.log('Push to release branch')
+await git.push(['--force'])
+
+console.log('Checkout main branch')
+await git.checkout('main')
