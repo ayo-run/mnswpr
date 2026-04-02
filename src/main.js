20bed545e1e47e1288228890719d89b467585bc4
Ayo <ayo@ayco.io>
2026-04-02 12:12:39 +0200

    refactor: use vite public dir

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './index.css';
 import './modules/loading/loading.css';
-import './assets/favicon.ico';
+import '../public/favicon.ico';
 import Minesweeper from './minesweeper.js';
 
 const myMinesweeper = new Minesweeper('app');
