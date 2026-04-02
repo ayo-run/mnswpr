139d85001c52393822b6e9086d06ee016eb9a423
Ayo <ayo@ayco.io>
2026-04-02 16:52:24 +0200

    chore: use eslint for css linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,29 @@
+// @ts-check
+
 import js from '@eslint/js'
+import css from '@eslint/css'
 import globals from 'globals'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
+  {
+    files: ['**/*.css'],
+    plugins: {
+      css
+    },
+    languageOptions: {
+      tolerant: true
+    },
+    language: 'css/css',
+    rules: {
+      'css/no-duplicate-imports': 'error',
+      'css/no-empty-blocks': 'error',
+      'css/no-invalid-at-rules': 'error',
+      'css/no-invalid-properties': 'error'
+    },
+    ignores: ['./src/modules/loading/loading.css']
+  },
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js, '@stylistic': stylistic},
