c09181f380e915071af7925136e1b8c038fc5851
Ayo <ayo@ayco.io>
2026-04-03 14:57:51 +0200

    feat(app): show leaderboard & enable sending result on game done

--- app/main.js
+++ app/main.js
@@ -2,22 +2,41 @@ import mnswpr from '@ayo-run/mnswpr/mnswpr.js'
 import '@ayo-run/mnswpr/mnswpr.css'
 import * as pkg from '@ayo-run/mnswpr/package.json'
 import { LoadingService } from '../utils/'
+import { LeaderBoardService } from './modules/leader-board/leader-board.js'
+
+const leaderBoardService = new LeaderBoardService()
 
 const version = import.meta.env.MODE === 'development'
   ? 'dev'
   : pkg.version
 
+const initializeGameBoard = async (level) => {
+  const prevousLeaderBoard = document.getElementById('leaderboard')
+  const loadingService = new LoadingService()
+  const loadingWrapper = document.createElement('div')
+  loadingWrapper.id = 'loading-wrapper'
+  loadingService.addLoading(loadingWrapper)
+
+  const appElement = document.getElementById('app')
+  if (prevousLeaderBoard){
+    const parent = prevousLeaderBoard.parentNode
+    parent.replaceChild(loadingWrapper, prevousLeaderBoard)
+  }else{
+    appElement.append(loadingWrapper)
+  }
+  const leaderBoardWrapper = await leaderBoardService.update(level.id, `Best Times (${level.name})`)
+  leaderBoardWrapper.id = 'leaderboard'
+
+  appElement.replaceChild(leaderBoardWrapper, loadingWrapper)
+}
+
+const sendGameResult = (game) => {
+  leaderBoardService.send(game, 'time')
+}
+
 const game = new mnswpr('app', version, {
-  levelChanged: () => console.log('[hook]: level reset'),
-  gameDone: (game) => console.log('[hook]: game done', game)
+  levelChanged: (level) => initializeGameBoard(level),
+  gameDone: (game) => sendGameResult(game)
 })
 game.initialize()
-
-const loadingService = new LoadingService()
-const loadingWrapper = document.createElement('div')
-loadingWrapper.id = 'loading-wrapper'
-loadingService.addLoading(loadingWrapper)
-
-const appElement = document.getElementById('app')
-appElement.append(loadingWrapper)
 
